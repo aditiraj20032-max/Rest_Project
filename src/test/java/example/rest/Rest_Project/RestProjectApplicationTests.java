d7f64bedf3d3b205d3be0fd0f283f391387dd8c3
aditiraj20032-max <aditiraj20032@gmail.com>
2025-08-01 16:39:56 +0530

    Update RestProjectApplicationTests.java

--- src/test/java/example/rest/Rest_Project/RestProjectApplicationTests.java
+++ src/test/java/example/rest/Rest_Project/RestProjectApplicationTests.java
@@ -3,7 +3,7 @@ package example.rest.Rest_Project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RestProjectApplication.class)
 class RestProjectApplicationTests {
 
 	@Test
